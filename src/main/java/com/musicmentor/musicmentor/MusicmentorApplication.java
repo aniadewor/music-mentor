a265915ca40633beb24eed86339fdc9f1fdb2925
aniadewor <116655947+aniadewor@users.noreply.github.com>
2025-01-03 16:46:58 +0100

    Initial commit

---
+++ src/main/java/com/musicmentor/musicmentor/MusicmentorApplication.java
@@ -0,0 +1,13 @@
+package com.musicmentor.musicmentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicmentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicmentorApplication.class, args);
+	}
+
+}
