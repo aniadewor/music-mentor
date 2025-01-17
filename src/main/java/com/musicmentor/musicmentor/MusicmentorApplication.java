3caa0200f69d3b6ca8d0c55bb660341983bf1363
aniadewor <116655947+aniadewor@users.noreply.github.com>
2025-01-17 16:39:00 +0100

    Add Pgadmin

--- src/main/java/com/musicmentor/musicmentor/MusicmentorApplication.java
+++ src/main/java/com/musicmentor/musicmentor/MusicmentorApplication.java
@@ -4,19 +4,13 @@ import com.musicmentor.musicmentor.model.Role;
 import com.musicmentor.musicmentor.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.musicmentor.musicmentor.repository")
 @SpringBootApplication
 public class MusicmentorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicmentorApplication.class, args);
-		User user1 = new User();
-		user1.setId(1);
-		user1.setName("John Doe");
-		user1.setEmail("john.doe@gmail.com");
-		user1.setPassword("password");
-		user1.setRole(Role.STUDENT);
-		System.out.println(user1.getEmail());
 	}
-
 }
--- src/main/java/com/musicmentor/musicmentor/model/User.java
+++ src/main/java/com/musicmentor/musicmentor/model/User.java
@@ -1,16 +1,21 @@
 package com.musicmentor.musicmentor.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name="users")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String lastName;
@@ -18,66 +23,4 @@ public class User {
     private String password;
     private Role role;
 }
-
-//    public User(Integer id, String name, String email, String lastName, String password, Role role) {
-//        this.id = id;
-//        this.name = name;
-//        this.email = email;
-//        this.lastName = lastName;
-//        this.password = password;
-//        this.role = role;
-//    }
-//
-//    public User() {
-//
-//    }
-//
-//    public Integer getId() {
-//        return id;
-//    }
-//
-//    public void setId(Integer id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public Role getRole() {
-//        return role;
-//    }
-//
-//    public void setRole(Role role) {
-//        this.role = role;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//}
 
