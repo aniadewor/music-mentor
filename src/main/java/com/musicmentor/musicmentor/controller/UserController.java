0bdc1ca35082910e74d77a7fcb126757586bc5bc
aniadewor <116655947+aniadewor@users.noreply.github.com>
2025-01-28 21:00:12 +0100

    Create login method

--- src/main/java/com/musicmentor/musicmentor/controller/UserController.java
+++ src/main/java/com/musicmentor/musicmentor/controller/UserController.java
@@ -3,6 +3,7 @@ package com.musicmentor.musicmentor.controller;
 import com.musicmentor.musicmentor.model.Role;
 import com.musicmentor.musicmentor.model.User;
 import com.musicmentor.musicmentor.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -34,21 +35,36 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
+    @GetMapping("/loginUser")
+    public ResponseEntity<?> loginUser(@RequestBody String email, @RequestBody String password, HttpSession session) {
+        System.out.println("Received user: " + email);
+        boolean success = userService.loginUser(email, password, session);
+       if (success) {
+           return ResponseEntity.status(HttpStatus.OK).body(userService.getUserByEmail(email));
+       }
+       else {
+           return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+       }
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<Optional<User>> getUserById(@PathVariable Integer id) {
         Optional<User> user = userService.getUserById(id);
         if (user.isPresent()) {
             return ResponseEntity.status(HttpStatus.OK).body(user);
-        }
-         else {
-             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
 
     }
 
     @GetMapping("/email/{email}")
-    public User getUserByEmail(@PathVariable String email)
-    {
-        return userService.getUserByEmail(email);
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        User user = userService.getUserByEmail(email);
+        if (user != null) { // Sprawdzamy, czy użytkownik nie jest nullem
+            return ResponseEntity.status(HttpStatus.OK).body(user);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 }
--- src/main/java/com/musicmentor/musicmentor/service/UserService.java
+++ src/main/java/com/musicmentor/musicmentor/service/UserService.java
@@ -2,7 +2,10 @@ package com.musicmentor.musicmentor.service;
 
 import com.musicmentor.musicmentor.model.User;
 import com.musicmentor.musicmentor.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,9 +14,15 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    //@Autowired
+    //private PasswordEncoder passwordEncoder;
     public User registerUser(User user) {
-        //sprawdzenie czy e-mail użytkownika istnieje w bazie
-        //szyfrowanie hasła
+     if(getUserByEmail(user.getEmail())!=null){
+         throw new IllegalArgumentException("Email already exists");
+     }
+//     String encryptedPassword = passwordEncoder.encode(user.getPassword());
+//        user.setPassword(encryptedPassword);
+
         return userRepository.save(user);
     }
     public Optional<User> getUserById(Integer id) {
@@ -21,5 +30,18 @@ public class UserService {
     }
     public User getUserByEmail(String email) {
         return userRepository.findByEmail(email);
+    }
+    public boolean loginUser(String email, String password, HttpSession session) {
+        if(getUserByEmail(email) == null) {
+            throw new IllegalArgumentException("Email not found");
+        }
+        User user = getUserByEmail(email);
+        if(user.getPassword().equals(password)) {
+            session.setAttribute("user", user);
+        }
+        else {
+            throw new IllegalArgumentException("Password incorrect");
+        }
+        return true;
     }
 }
